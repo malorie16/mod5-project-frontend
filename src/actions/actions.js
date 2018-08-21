d2b51a8b1f7e545e46ef570171d4876353ddd861
Malorie <malorie18@gmail.com>
2018-08-21 14:05:38 -0400

    fixed path

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://limitless-reaches-60351.herokuapp.com/=/'
-const USER_URL = 'http://limitless-reaches-60351.herokuapp.com//users'
-const PANO_URL = 'http://limitless-reaches-60351.herokuapp.com//panos'
-const LIKES_URL = 'http://limitless-reaches-60351.herokuapp.com//likes'
-const COMMENTS_URL = 'http://limitless-reaches-60351.herokuapp.com//comments'
+const BASE_URL = 'https://limitless-reaches-60351.herokuapp.com/=/'
+const USER_URL = 'https://limitless-reaches-60351.herokuapp.com//users'
+const PANO_URL = 'https://limitless-reaches-60351.herokuapp.com//panos'
+const LIKES_URL = 'https://limitless-reaches-60351.herokuapp.com//likes'
+const COMMENTS_URL = 'https://limitless-reaches-60351.herokuapp.com//comments'
 
 export const createUser = (user) => {
   const options = {
@@ -268,7 +268,7 @@ export const addComment = (pano) => {
       body: JSON.stringify({comment: {comment: pano.comment, pano_id: pano.pano_id}})
      }
      return (dispatch) => {
-       fetch('http://limitless-reaches-60351.herokuapp.com/comments', options)
+       fetch('https://limitless-reaches-60351.herokuapp.com/comments', options)
        .then(r => r.json())
        .then(data => {
          dispatch({
